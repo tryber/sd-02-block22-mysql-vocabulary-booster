037242bcedc1bff65f2ce92fd47783d62bc831ec
Johnatas Henrique <johnatas.henrique@gmail.com>
2020-05-15 20:26:09 -0300

    Desafio 8

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT 
+  o.OrderID,
+  o.CustomerID,
+  c.ContactName,
+  o.ShipperID
+FROM w3schools.orders AS o
+INNER JOIN w3schools.customers AS c ON o.CustomerID = c.CustomerID
+WHERE o.ShipperID IN (1, 2);
