c0b07f0df1415ccd00086cd9c1145bed1e1532ae
guicgs <etohlif@protonmail.com>
2020-05-15 17:36:45 -0300

    desafio 3 concluído

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT
-	country_name,
-	IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão`
+country_name,
+IF(REGION_ID = 1, 'incluído', 'não incluído') AS `Status Inclusão`
 FROM hr.countries
 LIMIT 25;
---
+++ desafio3.sql
@@ -0,0 +1,5 @@
+SELECT
+JOB_TITLE,
+AVG(MAX_SALARY - MIN_SALARY) AS `Diferença média entre salários mínimos e máximos`
+FROM hr.jobs
+GROUP BY JOB_TITLE;
