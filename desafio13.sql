9e1fa6d101a0402f44b880098da0d3dfb848bc8d
guicgs <etohlif@protonmail.com>
2020-05-16 12:49:11 -0300

    desafio 12 concluído

---
+++ desafio13.sql
@@ -0,0 +1,12 @@
+SELECT 
+    P.ProductName, P.Price
+FROM
+    w3schools.products P
+WHERE
+    (SELECT 
+            SUM(O.Quantity)
+        FROM
+            w3schools.order_details O
+        WHERE
+            O.ProductID = P.ProductID) > 80
+ORDER BY P.ProductName;
