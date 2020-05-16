0cf905ce1fcb72e0abd256261054c3ed62e86635
guicgs <etohlif@protonmail.com>
2020-05-16 20:33:57 -0300

    desafio 18 concluído

---
+++ desafio18.sql
@@ -0,0 +1,17 @@
+USE hr;
+DELIMITER $$
+
+CREATE FUNCTION ExibirQuantidadePessoasContratadasPorMesEAno(mes INT, ano INT)
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE QuantidadeFuncionariosContratados INT;
+    SELECT COUNT(EMPLOYEE_ID)
+    FROM hr.employees
+    WHERE MONTH(HIRE_DATE) = mes AND YEAR(HIRE_DATE) = ano
+    INTO QuantidadeFuncionariosContratados;
+    RETURN QuantidadeFuncionariosContratados;
+END $$
+
+DELIMITER ;
+
+SELECT ExibirQuantidadePessoasContratadasPorMesEAno(6, 1987);
