99e2a63704afe29020e4516322096424e29c600f
guicgs <etohlif@protonmail.com>
2020-05-16 20:22:19 -0300

    desafio 17 com sintaxe mais simples

--- desafio17.sql
+++ desafio17.sql
@@ -1,7 +1,16 @@
 SELECT 
     EMPLOYEE_ID, 
-    CONCAT(DAY(START_DATE), '/', MONTH(START_DATE), '/', YEAR(START_DATE)) AS `Data Início`,
-    CONCAT(DAY(END_DATE), '/', MONTH(END_DATE), '/', YEAR(END_DATE)) AS `Data Rescisão`,
+    DATE_FORMAT(START_DATE, '%d/%m/%Y') AS `Data Início`,
+    DATE_FORMAT(END_DATE, '%d/%m/%Y') AS `Data Rescisão`,
     ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS `Anos trabalhados`
 FROM hr.job_history
 ORDER BY EMPLOYEE_ID;
+
+### OUTRA FORMA ###
+# SELECT 
+#     EMPLOYEE_ID, 
+#     CONCAT(DAY(START_DATE), '/', MONTH(START_DATE), '/', YEAR(START_DATE)) AS `Data Início`,
+#     CONCAT(DAY(END_DATE), '/', MONTH(END_DATE), '/', YEAR(END_DATE)) AS `Data Rescisão`,
+#     ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS `Anos trabalhados`
+# FROM hr.job_history
+# ORDER BY EMPLOYEE_ID;
