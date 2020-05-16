a837371cfe23bd3ff6416f5c2e0beadc2904d558
richardwellerson <richardwellerson@gmail.com>
2020-05-15 22:27:57 -0300

    Corrigindo ordenaçao por crase

--- desafios/desafio6.sql
+++ desafios/desafio6.sql
@@ -1,7 +1,7 @@
 select
 jh.start_date,
 jh.employee_id,
-concat(ey.first_name, ' ', ey.last_name) as `Nome Completo`,
+concat(ey.first_name, ' ', ey.last_name) as 'Nome Completo',
 jh.job_id,
 jb.job_title as 'Cargo',
 dpt.department_id,
@@ -11,4 +11,4 @@ hr.job_history as jh
 inner join hr.jobs as jb on jh.job_id = jb.job_id
 inner join hr.employees as ey on jh.employee_id = ey.employee_id
 inner join hr.departments as dpt on ey.department_id = dpt.department_id
-order by `Nome Completo` desc;
+order by 3 desc;
