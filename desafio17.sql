622b1c3b6e86233178eae58bc83398c7646886da
guicgs <etohlif@protonmail.com>
2020-05-16 20:15:00 -0300

    desafio 17 concluído

---
+++ desafio17.sql
@@ -0,0 +1,7 @@
+SELECT 
+    EMPLOYEE_ID, 
+    CONCAT(DAY(START_DATE), '/', MONTH(START_DATE), '/', YEAR(START_DATE)) AS `Data Início`,
+    CONCAT(DAY(END_DATE), '/', MONTH(END_DATE), '/', YEAR(END_DATE)) AS `Data Rescisão`,
+    ROUND(DATEDIFF(END_DATE, START_DATE) / 365, 2) AS `Anos trabalhados`
+FROM hr.job_history
+ORDER BY EMPLOYEE_ID;
