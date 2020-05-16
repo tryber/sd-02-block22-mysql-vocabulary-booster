300a9057e8c89ef0ca0e4e5a482af864efd44b40
guicgs <etohlif@protonmail.com>
2020-05-16 14:03:07 -0300

    CC

--- desafio15.sql
+++ desafio15.sql
@@ -2,7 +2,6 @@ USE hr;
 DELIMITER $$
 
 DROP PROCEDURE IF EXISTS BuscarMediaPorCategoriaDeTrabalho $$
-
 CREATE PROCEDURE BuscarMediaPorCategoriaDeTrabalho(IN id VARCHAR(10))
 BEGIN
 	SELECT ROUND(AVG(SALARY), 2) AS media
