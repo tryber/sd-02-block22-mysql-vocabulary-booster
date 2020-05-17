a2448ce60642f869f1ec6b6a275b5aaab37fb779
richardwellerson <richardwellerson@gmail.com>
2020-05-17 18:34:59 -0300

    Correçao do desafio 12 usando self join

--- desafios/desafio12.sql
+++ desafios/desafio12.sql
@@ -5,6 +5,7 @@ emp1.phone_number,
 emp2.first_name,
 emp2.salary,
 emp2.phone_number
-from hr.employees as emp1
-inner join hr.employees as emp2 on emp1.job_id = emp2.job_id
+from hr.employees as emp1,
+hr.employees as emp2
+where emp1.job_id = emp2.job_id
 order by 1;
