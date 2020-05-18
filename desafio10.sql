bc8cbaf37701b08d2457308015035c564998db67
mateustalles <mateustalles@gmail.com>
2020-05-18 08:42:49 -0300

    Desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,10 @@
+SELECT od.ProductID, MIN(od.Quantity) AS Mínimo, MAX(od.Quantity) AS Máximo, 
+	(SELECT TRUNCATE(AVG(od.Quantity), 2) 
+    FROM w3schools.order_details
+    WHERE ProductID = od.ProductId
+    AND TRUNCATE(AVG(od.Quantity), 2) > 20.00
+    GROUP BY ProductID) AS Média
+FROM w3schools.order_details AS od
+GROUP BY od.ProductID
+HAVING Média IS NOT NULL
+ORDER BY Média ASC
