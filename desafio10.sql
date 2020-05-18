92a8c8e0c62dd67c1dc39d8021e8fe13f768822f
douglas-he <doughrqe@gmail.com>
2020-05-18 01:54:28 -0300

    desafio10

---
+++ desafio10.sql
@@ -0,0 +1,13 @@
+SELECT
+  ProductID,
+  MIN(Quantity) AS 'Mínimo',
+  Max(Quantity) AS 'Máximo',
+  ROUND(AVG(Quantity), 2) AS 'Média'
+FROM
+  w3schools.order_details
+GROUP BY
+  ProductID
+HAVING
+  `Média` > 20
+ORDER BY
+  `Média`;
