0f83930257fff5488f0571af866da602d8b23f74
richardwellerson <richardwellerson@gmail.com>
2020-05-17 21:34:52 -0300

    Correçao desafio 16

--- desafios/desafio16.sql
+++ desafios/desafio16.sql
@@ -4,8 +4,8 @@ create function BuscarQuantidadeDeEmpregosPorFuncionario(employeeID int)
 returns int reads sql data
 begin
 declare final_value INT;
-	SELECT COUNT(job_id) from job_history where employee_id = employeeid into final_value;
-    return final_value;
+SELECT COUNT(job_id) from job_history where employee_id = employeeid into final_value;
+return final_value;
 end $$
 
 DELIMITER ;
