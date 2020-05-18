df3af9892ec49334e5e093c64df8adb07e8838d7
guicgs <etohlif@protonmail.com>
2020-05-18 12:15:23 -0300

    adequando requisito 14 à nova interpretação

--- desafio14.sql
+++ desafio14.sql
@@ -1,5 +1,3 @@
-(SELECT Country FROM w3schools.customers)
+(SELECT Country FROM w3schools.customers ORDER BY Country LIMIT 5)
 UNION
-(SELECT Country FROM w3schools.suppliers)
-ORDER BY Country
-LIMIT 5;
+(SELECT Country FROM w3schools.suppliers ORDER BY Country LIMIT 5);
