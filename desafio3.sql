8ff27f63854de34a098a6430770fad939571349a
guicgs <etohlif@protonmail.com>
2020-05-18 13:21:49 -0300

    colocando order by requisito 3

--- desafio3.sql
+++ desafio3.sql
@@ -3,4 +3,5 @@ SELECT
     AVG(MAX_SALARY - MIN_SALARY) AS `Diferença média entre salários mínimos e máximos`
 FROM
     hr.jobs
-GROUP BY JOB_TITLE;
+GROUP BY JOB_TITLE
+ORDER BY `Diferença média entre salários mínimos e máximos`;
