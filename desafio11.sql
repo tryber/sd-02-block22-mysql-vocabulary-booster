a9512b1e643926068fc251eed90f1e8dabd47dca
guicgs <etohlif@protonmail.com>
2020-05-19 20:26:45 -0300

    desafio 11 corrigido

--- desafio11.sql
+++ desafio11.sql
@@ -2,11 +2,13 @@ SELECT
     A.CustomerID,
     A.CustomerName AS Nome,
     A.Country AS País,
-    COUNT(B.Country) - 1 AS `Número de Compatriotas`
+    COUNT(B.Country) AS `Número de Compatriotas`
 FROM
     w3schools.customers A,
     w3schools.customers B
 WHERE
     A.Country = B.Country
+        AND A.CustomerID <> B.CustomerID
 GROUP BY A.CustomerID
+HAVING `Número de Compatriotas` > 0
 ORDER BY A.CustomerName;
