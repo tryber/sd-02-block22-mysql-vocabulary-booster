5202c46ef6d60e25607b59eddf75a9ac842d9bec
Andrey <andreytsuzuki@gmail.com>
2020-05-19 13:45:26 -0300

    'fix'

--- desafio12.sql
+++ desafio12.sql
@@ -2,9 +2,9 @@ select
 e1.first_name,
 e1.salary,
 e1.phone_number,
-e2.first_name
-from hr.employees e1
-join hr.employees as e2 on e1.employee_id = e2.employee_id
-group by e1.first_name
+e2.first_name,
+e2.salary,
+e2.phone_number
+from hr.employees as e1, hr.employees as e2
 order by e1.first_name
 limit 1839;
