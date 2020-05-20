57ad5136a6834c0da2db7bead383bd9993321f89
eddyeboy27 <lipe0503@gmail.com>
2020-05-20 02:54:45 -0300

    Finalizado desafio 2

--- desafios/desafio2.sql
+++ desafios/desafio2.sql
@@ -1,13 +1,17 @@
--- Queremos categorizar o nível de cada cargo por salário.
--- Através do uso de condicionais, monte uma query que exiba, utilizando a tabela jobs do banco hr,
--- primeiramente o job_title e, em uma segunda coluna, o nível do salário de acordo com a informação de max_salary,
--- apelidando essa coluna de "Nível". Categorize os níveis de remuneração de acordo com a tabela abaixo.
--- Sua query deve produzir 19 resultados, e as colunas devem ser nomeadas assim como na imagem de referência:
+-- Foi decidido que vamos continuar atendendo apenas à região 1.Sendo assim,
+-- usando a tabela countries do banco hr como referência,
+-- precisamos de um relatório em que deve ser exibido o nome do país na coluna country_name e uma
+-- segunda coluna apelidada de Status Inclusão,
+-- que exibirá o status do país.
+-- Essa segunda coluna deve ser gerada com base no valor da coluna region_id e seu valor deve ser "incluído" ou "não incluído".
+-- Utilize os comandos condicionais IF ou CASE
+-- para gerar exatamente 25 resultados, como na imagem a seguir:
 
--- 5000 - 10000 - Baixo
-
--- 10001 - 20000 - Médio
-
--- 20001 - 30000 - Alto
-
--- Acima de 30000 - Altíssimo
+SELECT job_title, max_salary,
+  CASE
+    WHEN max_salary BETWEEN 5000 AND 10000 THEN 'Baixo'
+    WHEN max_salary BETWEEN 10001 AND 20000 THEN 'Médio'
+    WHEN max_salary BETWEEN 20001 AND 30000 THEN 'Alto'
+    ELSE 'Altíssimo'
+  END AS 'Nível'
+FROM hr.jobs;
