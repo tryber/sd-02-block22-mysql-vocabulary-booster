194d6662d9221d5735710e6fd449aa71322699cc
eddyeboy27 <lipe0503@gmail.com>
2020-05-22 02:19:24 -0300

    Finalizado Req 14

---
+++ desafios/desafio14.sql
@@ -0,0 +1,27 @@
+-- O banco w3schools possui países cadastrados nas tabelas customers e suppliers.
+-- Queremos saber quais são os cinco primeiros países que estão cadastrados no banco,
+-- em ordem alfabética, em qualquer uma das tabelas.Crie uma query que retorne esse resultado.
+
+(
+  SELECT
+    Country
+  FROM
+    w3schools.customers
+  ORDER BY
+    Country
+  LIMIT
+    5
+)
+UNION
+(
+  SELECT
+    Country
+  FROM
+    w3schools.suppliers
+  ORDER BY
+    Country
+  LIMIT
+    5
+)
+LIMIT
+  5;
