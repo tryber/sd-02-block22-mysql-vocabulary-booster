5ef06b7e5c94ecbb4303762abeb5ecb823ec2aa7
eddyeboy27 <lipe0503@gmail.com>
2020-05-22 04:10:26 -0300

    Finalizado Req 15

--- desafios/desafio15.sql
+++ desafios/desafio15.sql
@@ -1,22 +1,20 @@
--- Crie uma função chamada BuscarQuantidadeDeEmpregosPorFuncionario que,
--- ao receber o id de um funcionário, vá até a tabela job_history, no banco de dados hr e retorne a
--- quantidade de empregos já gravados no sistema relacionados a esse funcionário. 
--- Confirme que sua function retorna o valor 2 ao ser chamada passando um funcionário com id 101.
+-- Crie uma procedure chamada BuscarMediaPorCategoriaDeTrabalho que recebe um parâmetro VARCHAR
+-- correspondente a um job_id da tabela employees do banco de dados hr.Ao ser executada,
+-- sua procedure deverá obter da tabela employees a média salarial arredondada deste trabalho,
+-- arredondada para as duas casas decimais.Dica importante: não esqueça de incluir a linha USE hr;
+-- antes da declaração de sua procedure.Quando sua procedure for executada com parâmetro 'IT_PROG',
+-- deve retornar 5760.00.
 
 USE hr;
 
-DELIMITER $ $ CREATE FUNCTION BuscarQuantidadeDeEmpregosPorFuncionario(employee_id int)
-RETURNS INT READS SQL DATA 
-BEGIN 
-DECLARE 
-  full_value INT;
+DELIMITER $ $ CREATE PROCEDURE BuscarMediaPorCategoriaDeTrabalho(IN job_id VARCHAR(100)) BEGIN
 SELECT
-  COUNT(*)
+  ROUND(AVG(salary), 2)
 FROM
-  hr.job_history
+  hr.employees
 WHERE
-  hr.job_history.employee_id = employee_id INTO full_value;
-RETURN full_value;
+  hr.employees.job_id = job_id;
+
 END $ $ DELIMITER;
-SELECT
-  BuscarQuantidadeDeEmpregosPorFuncionario(101);
+
+CALL BuscarMediaPorCategoriaDeTrabalho('IT_PROG');
