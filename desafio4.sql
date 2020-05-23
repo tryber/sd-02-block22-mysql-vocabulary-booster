08a44f80ad75e3f73042d1c7e1c1dd7fc14060ed
miguel-miranda <ayandram7727@gmail.com>
2020-05-22 22:06:22 -0300

    CC

--- desafio4.sql
+++ desafio4.sql
@@ -3,7 +3,7 @@ SELECT
   AVG(salary) AS `Média`,
     CASE
       WHEN AVG(salary) BETWEEN 2000 AND 5800 THEN 'Junior'
-	  WHEN AVG(salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
+      WHEN AVG(salary) BETWEEN 5801 AND 7500 THEN 'Pleno'
       WHEN AVG(salary) BETWEEN 7501 AND 10500 THEN 'Sênior'
     ELSE 'CEO'
   END AS `Senioridade`
