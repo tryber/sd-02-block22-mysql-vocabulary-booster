0b5892bf3a5d09b1212949cb066d0411055c09d7
miguel-miranda <ayandram7727@gmail.com>
2020-05-25 12:38:01 -0300

    Feitos todos os requisitos bônus

---
+++ desafio19.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ExibirHistoricoCompletoPorFuncionario $$
+
+CREATE PROCEDURE ExibirHistoricoCompletoPorFuncionario(IN employee_id INT)
+BEGIN
+  SELECT
+    JH.EMPLOYEE_ID,
+    CONCAT(E.FIRST_NAME, ' ', E.LAST_NAME) AS `Nome Completo`,
+    J.JOB_TITLE AS Cargo,
+    D.DEPARTMENT_NAME AS Departamento
+  FROM hr.job_history AS JH
+  INNER JOIN hr.employees AS E
+  ON E.EMPLOYEE_ID = JH.EMPLOYEE_ID
+  INNER JOIN hr.jobs AS J
+  ON J.JOB_ID = JH.JOB_ID
+  INNER JOIN hr.departments AS D
+  ON D.DEPARTMENT_ID = JH.DEPARTMENT_ID
+  WHERE JH.EMPLOYEE_ID = employee_id;
+END $$
+
+DELIMITER ;
+
+CALL ExibirHistoricoCompletoPorFuncionario(101);
