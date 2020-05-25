f58ada14c12bf3f34084189b40bbe9fcf50728db
richardwellerson <richardwellerson@gmail.com>
2020-05-25 13:36:52 -0300

    Correçao desafio 8

--- desafios/desafio8.sql
+++ desafios/desafio8.sql
@@ -5,5 +5,5 @@ cust.contactName,
 ords.shipperid
 from w3schools.orders AS ords
 inner join w3schools.customers as cust on cust.customerid = ords.customerid
-where shipperid < 3
+where shipperid in (1, 2)
 order by cust.customerid;
