51656f40959f36c1af56138d4430fb25adc5df77
mateustalles <mateustalles@gmail.com>
2020-05-25 14:17:33 -0300

    Correção desafio 3

--- desafio3.sql
+++ desafio3.sql
@@ -1,3 +1,4 @@
 SELECT job_title, AVG(max_salary - min_salary) AS 'Diferença média entre salários mínimos e máximos'
 FROM hr.jobs
-GROUP BY job_title;
+GROUP BY job_title
+ORDER BY `Diferença média entre salários mínimos e máximos`;
