72b32788c75f6bb55c9940f5959188a133e6af28
richardwellerson <richardwellerson@gmail.com>
2020-05-25 13:56:10 -0300

    Correçao desafio 3

--- desafios/desafio3.sql
+++ desafios/desafio3.sql
@@ -1,3 +1,6 @@
-select job_title, round(max_salary - min_salary, 4)
+select
+job_title, cast(AVG(MAX_SALARY) - AVG(MIN_SALARY) as DECIMAL(10,4))
 as "Diferença média entre salários mínimos e máximos"
-from hr.jobs;
+from hr.jobs
+group by job_title
+order by 2;
