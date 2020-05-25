33e19208ce89e6329be86185be7419b6f499c066
mateustalles <mateustalles@gmail.com>
2020-05-25 14:22:51 -0300

    Correção desafio 7

--- desafio7.sql
+++ desafio7.sql
@@ -2,4 +2,4 @@ SELECT jh.employee_id, UPPER(CONCAT(emp.first_name, ' ', emp.last_name)) AS 'Nom
     jh.start_date, emp.salary
 FROM hr.job_history AS jh, hr.employees AS emp
 WHERE MONTH(jh.start_date) IN (1, 2, 3) AND jh.EMPLOYEE_ID = emp.employee_id
-ORDER BY emp.first_name ASC;
+ORDER BY `Nome Completo`;
