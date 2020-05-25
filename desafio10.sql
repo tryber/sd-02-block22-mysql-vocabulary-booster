94f86a1e3f30ccaadb20b7d680a7bfed6756264e
mateustalles <mateustalles@gmail.com>
2020-05-25 14:24:31 -0300

    Correção desafio 10

--- desafio10.sql
+++ desafio10.sql
@@ -1,8 +1,8 @@
 SELECT od.ProductID, MIN(od.Quantity) AS Mínimo, MAX(od.Quantity) AS Máximo, 
-    (SELECT TRUNCATE(AVG(od.Quantity), 2) 
+    (SELECT ROUND(AVG(od.Quantity), 2) 
     FROM w3schools.order_details
     WHERE ProductID = od.ProductId
-    AND TRUNCATE(AVG(od.Quantity), 2) > 20.00
+    AND ROUND(AVG(od.Quantity), 2) > 20.00
     GROUP BY ProductID) AS Média
 FROM w3schools.order_details AS od
 GROUP BY od.ProductID
